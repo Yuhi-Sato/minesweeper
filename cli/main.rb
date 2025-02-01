9223fe3ba3201ce0ba0da14c64ffbbdb21e2d34d
Yuhi-Sato <yuhi120101@gmail.com>
2025-02-01 18:02:32 +0900

    refactor: 💡 rescue処理を追加

--- cli/main.rb
+++ cli/main.rb
@@ -22,42 +22,46 @@ if __FILE__ == $0
   game = Domains::Minesweeper.new
 
   until game.finished?
-    game.board.display
-
-    input = prompt_command
-    command, x_str, y_str = parse_command(input)
-
-    case command
-    when "reveal", "r"
-      if x_str && y_str
-        x, y = x_str.to_i, y_str.to_i
-        game.reveal_cell(x, y)
-      else
-        puts "座標が正しくありません。例: 'reveal 2 3'"
-      end
-    when "flag", "f"
-      if x_str && y_str
-        x, y = x_str.to_i, y_str.to_i
-        game.toggle_flag(x, y)
-      else
-        puts "座標が正しくありません。例: 'flag 1 1'"
-      end
-    when "exit", "e"
-      puts "ゲームを終了します。"
-      exit
-    else
-      puts "不明なコマンドです。'reveal x y', 'flag x y', 'exit'を使用してください。"
-    end
-
-    if game.finished?
-      puts "\nゲーム終了！"
+    begin
       game.board.display
-      if game.board.bombed?
-        puts "爆弾を開いてしまいました…"
+
+      input = prompt_command
+      command, x_str, y_str = parse_command(input)
+
+      case command
+      when "reveal", "r"
+        if x_str && y_str
+          x, y = x_str.to_i, y_str.to_i
+          game.reveal_cell(x, y)
+        else
+          puts "座標が正しくありません。例: 'reveal 2 3'"
+        end
+      when "flag", "f"
+        if x_str && y_str
+          x, y = x_str.to_i, y_str.to_i
+          game.toggle_flag(x, y)
+        else
+          puts "座標が正しくありません。例: 'flag 1 1'"
+        end
+      when "exit", "e"
+        puts "ゲームを終了します。"
+        exit
       else
-        puts "おめでとうございます！地雷を回避して全てのセルを開きました！"
+        puts "不明なコマンドです。'reveal x y', 'flag x y', 'exit'を使用してください。"
       end
-      break
+
+      if game.finished?
+        puts "\nゲーム終了！"
+        game.board.display
+        if game.board.bombed?
+          puts "爆弾を開いてしまいました…"
+        else
+          puts "おめでとうございます！地雷を回避して全てのセルを開きました！"
+        end
+        break
+      end
+    rescue
+      puts "エラーが発生しました。もう一度入力してください。"
     end
   end
 end
