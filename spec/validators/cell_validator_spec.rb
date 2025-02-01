32fe61eb56cf7a315f6b13b1e792af5d660eea82
Yuhi-Sato <yuhi120101@gmail.com>
2025-02-01 22:27:19 +0900

    test: 💍 cell_validatorのテスト

---
+++ spec/validators/cell_validator_spec.rb
@@ -0,0 +1,29 @@
+RSpec.describe Domains::Validators::CellValidator do
+  describe '#validate_toggle_flag!' do
+    context 'セルが既に開かれているとき' do
+      let(:cell) { build(:cell) }
+
+      before do
+        cell.reveal
+      end
+
+      it 'Domains::Validators::Errorが発生すること' do
+        expect { cell.reveal }.to raise_error(Domains::Validators::Error)
+      end
+    end
+  end
+
+  describe '#validate_reveal!' do
+    context 'セルが既に開かれているとき' do
+      let(:cell) { build(:cell) }
+
+      before do
+        cell.reveal
+      end
+
+      it 'Domains::Validators::Errorが発生すること' do
+        expect { cell.reveal }.to raise_error(Domains::Validators::Error)
+      end
+    end
+  end
+end
