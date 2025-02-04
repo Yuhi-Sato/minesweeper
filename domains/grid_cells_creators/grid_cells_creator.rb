6f0a55d7911b91b193d53a489a3e80a1031f8c7e
Yuhi-Sato <yuhi120101@gmail.com>
2025-02-05 05:47:54 +0900

    fix: 🐛 diagram修正

--- domains/grid_cells_creators/grid_cells_creator.rb
+++
@@ -1,9 +0,0 @@
-module Domains
-  module GridCellsCreators
-    class Base
-      def create
-        raise NotImplementedError, "#{self.class}##{__method__} is must be implemented"
-      end
-    end
-  end
-end
