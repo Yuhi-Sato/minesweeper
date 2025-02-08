119a37ee7ff685645b037e1addf9e9e63ab03331
Yuhi-Sato <yuhi120101@gmail.com>
2025-02-08 12:31:12 +0900

    refactor: 💡 objectに振る舞いを委譲した

--- domains/validators/cell_validator.rb
+++ domains/validators/cell_validator.rb
@@ -1,15 +1,20 @@
 # frozen_string_literal: true
 
+require 'forwardable'
+
 module Domains
   module Validators
     class CellValidator < Base
+      extend Forwardable
+      def_delegators :@object, :revealed?
+
       def validate_toggle_flag!
-        errors.add('Cannot toggle_flag a revealed cell') if object.revealed?
+        errors.add('Cannot toggle_flag a revealed cell') if revealed?
         raise Error, errors.full_messages unless errors.empty?
       end
 
       def validate_reveal!
-        errors.add('Cannot reveal a revealed cell') if object.revealed?
+        errors.add('Cannot reveal a revealed cell') if revealed?
         raise Error, errors.full_messages unless errors.empty?
       end
     end
