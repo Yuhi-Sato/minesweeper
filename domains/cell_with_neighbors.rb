7482794bc2e0c941ec802655c74c451163f5ddb1
Yuhi-Sato <yuhi120101@gmail.com>
2025-04-05 21:42:32 +0900

    refactor: 💡 cell_with_neighborsのrbs-inline

--- domains/cell_with_neighbors.rb
+++ domains/cell_with_neighbors.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+# rbs_inline: enabled
 
 require 'forwardable'
 
@@ -6,25 +7,40 @@ module Domains
   class CellWithNeighbors < Base
     extend Forwardable
 
-    attr_reader :neighbor_bomb_cell_count
+    # @rbs @base: Cell
+    # @rbs @neighbors: Array[CellWithNeighbors]
+    # @rbs @neighbor_bomb_cell_count: Integer
 
-    def initialize(base:, neighbors: [])
+    attr_reader :neighbor_bomb_cell_count #: Integer
+
+    # @rbs (base: Cell, neighbors: Array[CellWithNeighbors]) -> void
+    def initialize(base:, neighbors:)
       @base = base
-      @neighbors = neighbors
+      @neighbors = neighbors || []
       @neighbor_bomb_cell_count = count_neighbor_bomb_cell
     end
 
+    # @rbs!
+    #   def bomb?: () -> bool
+    #   def empty?: () -> bool
+    #   def flag?: () -> bool
+    #   def revealed?: () -> bool
+    #   def toggle_flag: () -> void
+    #   def reveal: () -> void
     def_delegators :@base, :bomb?, :empty?, :flag?, :revealed?, :toggle_flag, :reveal
 
+    # @rbs () -> Array[CellWithNeighbors]
     def neighbors
       @neighbors.dup
     end
 
+    # @rbs (neighbor: CellWithNeighbors) -> void
     def add_neighbor(neighbor:)
       @neighbors << neighbor
       @neighbor_bomb_cell_count += 1 if neighbor.bomb?
     end
 
+    # @rbs () -> void
     def reveal_with_neighbors
       reveal
 
@@ -39,12 +55,14 @@ module Domains
       end
     end
 
+    # @rbs () -> Integer
     def count_revealed_cell
       neighbors.count(&:revealed?)
     end
 
     private
 
+    # @rbs () -> Integer
     def count_neighbor_bomb_cell
       neighbors.count(&:bomb?)
     end
